bd0016ded66250e2531ed2a8db775e7a25ab1ee6
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-04-05 11:43:21 -0300

    Service layer, component registration

--- src/main/java/edu/mfvp/course/resources/UserResource.java
+++ src/main/java/edu/mfvp/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package edu.mfvp.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.mfvp.course.entities.User;
+import edu.mfvp.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Matheus", "matheus@gmail.com", "99989999", "1234");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User u = service.findById(id);
 		return ResponseEntity.ok().body(u);
 	}
 
