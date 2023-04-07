aeed0e275b5e83cfbf599100c5cd3c27a77e01a2
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-04-07 15:01:18 -0300

    Payment, one-to-one association

--- src/main/java/edu/mfvp/course/config/TestConfig.java
+++ src/main/java/edu/mfvp/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import edu.mfvp.course.entities.Category;
 import edu.mfvp.course.entities.Order;
 import edu.mfvp.course.entities.OrderItem;
+import edu.mfvp.course.entities.Payment;
 import edu.mfvp.course.entities.Product;
 import edu.mfvp.course.entities.User;
 import edu.mfvp.course.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
