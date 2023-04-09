315ce7749a39889a075456c15aaf380bb94ced72
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-04-09 20:13:50 -0300

    Exception handling - findById

--- src/main/java/edu/mfvp/course/services/UserService.java
+++ src/main/java/edu/mfvp/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import edu.mfvp.course.entities.User;
 import edu.mfvp.course.repositories.UserRepository;
+import edu.mfvp.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
