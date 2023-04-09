a910f856dbebe3925ec1fe117816922b27422f6c
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-04-09 20:51:53 -0300

    Exception handling - update

--- src/main/java/edu/mfvp/course/services/UserService.java
+++ src/main/java/edu/mfvp/course/services/UserService.java
@@ -12,6 +12,7 @@ import edu.mfvp.course.entities.User;
 import edu.mfvp.course.repositories.UserRepository;
 import edu.mfvp.course.services.exceptions.DatabaseException;
 import edu.mfvp.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -44,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
